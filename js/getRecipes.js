db09ba185728272ee84fe30d22661ce6a61b10cb
apple <apple@apples-MacBook-Pro.local>
2025-07-10 18:16:48 +0530

    implemented the recipe filter

--- js/getRecipes.js
+++ js/getRecipes.js
@@ -1,6 +1,7 @@
 const recipeListContainer = document.getElementById('recipes');
+let allRecipes = [];
 
-//get data using async await
+
 async function getRecipes() {
     const apiUrl = "https://dummyjson.com/recipes";
     const recipeContainer = document.getElementById('recipes');
@@ -12,48 +13,86 @@ async function getRecipes() {
 
         const response = await fetch(apiUrl);
         if(!response.ok) {
-            throw new Error('Error while fetching data');
+            throw new Error('Error while fetching the data');
         }
+
         const data = await response.json();
         console.log(data);
         
-        recipeContainer.innerHTML = '';
-
-        data.recipes.forEach(recipe => {
-            const recipeCard = document.createElement('div');
-            recipeCard.classList.add('col-md-3');
-
-            recipeCard.innerHTML = `
-                <div class="card shadow-sm rounded-4 mb-4">
-                    <div class="card-body p-2">
-                        <figure class="recipe-thumb rounded-4 overflow-hidden d-flex align-items-center jusitfy-content-center position-relative">
-                            <img src="${recipe.image}" alt="${recipe.name}" class="img-fluid">
-                            <span class="position-absolute top-0 end-0 me-2 mt-2 bg-secondary border shadow-sm text-white small rounded-pill px-2">${recipe.cuisine}</span>
-                        </figure>
-                        <div class="recipe-quick-info px-3 pb-3">
-                            <p class="mb-0 text-black-50">${recipe.mealType.join(', ')}</p>
-                            <h5 class="text-truncate">${recipe.name}</h5>
-                            <div class="d-flex align-items-center justify-content-between">
-                                <h5 class="text-secondary m-0">${recipe.cookTimeMinutes} min</h5>
-                                <p class="m-0 text-black-50">
-                                    <i class="bi bi-star-fill text-warning"></i> ${recipe.rating}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `;
-
-            recipeContainer.appendChild(recipeCard);
-        })
-    } catch(error) {
-        console.log(error);
-        recipeContainer.innerHTML = `<p class="m-0 text-danger">Failed to load recipes.</p>`        
+        allRecipes = data.recipes;
+        renderRecipies(allRecipes);
+        
+    } catch (error) {
+        console.log('Fetch Error', error);
+        recipeContainer.innerHTML = `<p>Failed to load the recipe</p>`        
     } finally {
         loader.style.display = 'none';
         recipeContainer.style.display = 'flex';
     }
 }
+
+
+function renderRecipies(recipes) {
+    const recipeContainer = document.getElementById('recipes');
+    const recipeCount = document.getElementById('recipe-count');
+    recipeContainer.innerHTML = '';
+
+    recipeCount.textContent = `${recipes.length} recipe${recipes.length !== 1 ? 's' : ''} found`;
+
+    if(recipes.length === 0) {
+        recipeContainer.innerHTML = `<p>No recipes found.</p>`;
+        return;
+    }
+
+    recipes.forEach(recipe => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add('col-md-3');
+
+        recipeCard.innerHTML = `
+            <div class="card shadow-sm rounded-4 mb-4">
+                <div class="card-body p-2">
+                    <figure class="recipe-thumb rounded-4 overflow-hidden d-flex align-items-center jusitfy-content-center position-relative">
+                        <img src="${recipe.image}" alt="${recipe.name}" class="img-fluid">
+                        <span class="position-absolute top-0 end-0 me-2 mt-2 bg-secondary border shadow-sm text-white small rounded-pill px-2">${recipe.cuisine}</span>
+                    </figure>
+                    <div class="recipe-quick-info px-3 pb-3">
+                        <p class="mb-0 text-black-50">${recipe.mealType.join(', ')}</p>
+                        <h5 class="text-truncate">${recipe.name}</h5>
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h5 class="text-secondary m-0">${recipe.cookTimeMinutes} min</h5>
+                            <p class="m-0 text-black-50">
+                                <i class="bi bi-star-fill text-warning"></i> ${recipe.rating}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        recipeContainer.appendChild(recipeCard);
+    })
+}
+
+
+//handle search input
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', function() {
+    const searchTerm = this.value.toLowerCase();
+
+    const filteredRecipes = allRecipes.filter(recipe => {
+        const nameMatch = recipe.name.toLowerCase().includes(searchTerm);
+        const cuisineMatch = recipe.cuisine.toLowerCase().includes(searchTerm);
+        //const tagMatch = recipe.tags.some(tg => tg.toLowerCase().includes(searchTerm));
+        //const ingredientsMatch = recipe.ingredients.some(ing => ing.toLowerCase().includes(searchTerm));
+        return nameMatch || cuisineMatch;
+    })
+
+    console.log('Filtered Recipes', filteredRecipes);
+    
+
+    renderRecipies(filteredRecipes);
+})
+
 
 
 window.addEventListener('DOMContentLoaded', () => {
